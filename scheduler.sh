5bbb26d3a9c9a0536e874134c7c69444a0e48990
Yogesh <ywagh4430@gmail.com>
2025-01-17 16:24:58 -0600

    minor changes for testing

--- scheduler.sh
+++ scheduler.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 #SBATCH --job-name=time_Scheduling      ## Name of the job
-#SBATCH --output=time_scheduling.out    ## Output file
-#SBATCH --time=04:00:00                 ## Job Duration
+#SBATCH --output=gwemopt_milp.out    ## Output file
+#SBATCH --time=10:00:00                 ## Job Duration
 #SBATCH --ntasks=1                      ## Number of tasks (analyses) to run
-#SBATCH --cpus-per-task=16             ## Number of threads the code will use
-#SBATCH --mem-per-cpu=4G               ## Memory per CPU required by the job.
+#SBATCH --cpus-per-task=24             ## Number of threads the code will use
+#SBATCH --mem-per-cpu=6G               ## Memory per CPU required by the job.
 #SBATCH --account=bcrv-delta-cpu
 #SBATCH --partition=cpu
 eval "$(conda shell.bash hook)"   # Ensure conda shell functionality
 
 conda activate MILP_3                  ## Activate the conda environment
 
-srun python /u/ywagh/scheduler_results/slew_speed/MILP_time_probs_plot.py
-
+srun python /u/ywagh/GWEMOPT_MILP_csv.py 
